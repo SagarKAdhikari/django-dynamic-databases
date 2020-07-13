f51894003de53d52e5bdf993fa6e97f5054ed2f9
SagarKAdhikari <ska80117@gmail.com>
2020-07-13 09:22:40 +0545

    Update middleware.py

--- dbs/middleware.py
+++ dbs/middleware.py
@@ -18,18 +18,6 @@ class RouterMiddleware(object):
         print(request.body)
         print(request.method)
 
-
-        # print(request.user)
-
-        # if request.META.get("HTTP_AUTHORIZATION", "").startswith("Bearer"):
-        #     if not hasattr(request, "user") or request.user.is_anonymous:
-        #         print("ww")
-        #         user = authenticate(request=request)
-        #         print("J")
-        #         print(user)
-        #         if user:
-        #             request.user = request._cached_user = user
-
         print("A")
         print(hasattr(request,'user'))
         print("X")
@@ -37,7 +25,6 @@ class RouterMiddleware(object):
         print("B")
 
         print(request.META.get('HTTP_AUTHORIZATION'))
-
 
         if hasattr(request,'user'):
             if request.user and request.user.id:
@@ -51,62 +38,8 @@ class RouterMiddleware(object):
         print("ZZ")
         print(request.user)
 
-
-
-        # if request.user and request.user.id:
-
-
-        # print(request.META.get('HTTP_AUTHORIZATION'))
-        # token=request.META.get('HTTP_AUTHORIZATION').replace('Bearer ','')
-
-        # AccessToken.objects.get(token=token)
-
-        # print("X")
-        # print(AccessToken)
-
-        # print(AccessToken.user_id)
-        # print("Y")
-
-
-        # print("SDf")
-        # print(args)
-        # print(request.META)
-        # return None
-
-        # print(request.build_absolute_uri())
-
-        # request_cfg.cfg = 'db9815'
-        # try:
-        #     x=request.user
-        #     if request.user and request.user.username:
-        #         request_cfg.cfg='db'+str(request.user.username)
-        # except:
-        #     pass
-
         response = self.get_response(request)
         return response
-
-    
-    # def process_response( self, request, response ):
-    #     if hasattr( request_cfg, 'cfg' ):
-    #         del request_cfg.cfg
-    #     return response
-
-# def process_request(self, request):
-#         # do something only if request contains a Bearer token
-#         if request.META.get("HTTP_AUTHORIZATION", "").startswith("Bearer"):
-#             if not hasattr(request, "user") or request.user.is_anonymous:
-#                 user = authenticate(request=request)
-#                 if user:
-#                     request.user = request._cached_user = user
-#                 print(user)
-
-# def process_response(self, request, response):
-#     patch_vary_headers(response, ("Authorization",))
-#     return response
-
-
-
 
 
 class DatabaseRouter(object):
